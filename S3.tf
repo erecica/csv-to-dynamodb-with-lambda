96fccb03c26b2915db78d1e0528a4dcce36440ff
Edi Recica <edi@recica.nl>
2021-12-11 08:19:09 +0100

    Add comment

--- S3.tf
+++ S3.tf
@@ -1,3 +1,4 @@
+# Create a S3 bucket, where the csv file(s) are uploaded
 resource "aws_s3_bucket" "s3bucket" {
   bucket_prefix = "${var.aws_resource_name_tag}-"
   acl    = "private"
@@ -7,13 +8,15 @@ resource "aws_s3_bucket" "s3bucket" {
     enabled = false
   }
 }
-
+# Block public access control by default
 resource "aws_s3_bucket_public_access_block" "s3bucket" {
   bucket = aws_s3_bucket.s3bucket.id
   block_public_acls   = true
   block_public_policy = true
 }
 
+# Create Trigger and attach the Lambda function to the trigger. 
+# This trigger will be executed when the filter suffix ( in this case .csv file(s) ) is met.
 resource "aws_s3_bucket_notification" "s3-trigger" {
     bucket = aws_s3_bucket.s3bucket.id
 
@@ -22,5 +25,6 @@ resource "aws_s3_bucket_notification" "s3-trigger" {
         events              = ["s3:ObjectCreated:*"]
         filter_suffix       = ".csv"
     }
+    # Be sure to have the correct execution rights before creating the trigger
     depends_on = [aws_lambda_permission.allow_execution_form_bucket]
 }
